5fd4f5118ac51f63d86cda93420d09f97c758414
asciibox <obachmann@ymai.com>
2016-03-14 17:43:35 +0100

    Created nodeJS image creation for vga palette

--- assets/create_256_ansi_old.js
+++ assets/create_256_ansi_old.js
@@ -23,11 +23,38 @@
 /*jslint browser: true, devel: true, plusplus: true */
  var xStart = 0;
  var yStart = 0;
+ 
+   var ansicolors = [
+      '000000', 'aa0000', '00aa00', 'aa5500', '0000aa', 'aa00aa', '00aaaa', 'aaaaaa', '555555', 'ff5555', // 10
+      '55ff55', 'ffff55', '5555ff', 'ff55ff', '55ffff', 'ffffff', '000000', '00005f', '000087', '0000af', // 20
+      '0000D7', '0000FF', '005F00', '005F5F', '005f87', '005faf', '005fd7', '005fff', '008700', '00875f', // 30
+      '008787', '0087af', '0087d7', '0087ff', '00af00', '00af5f', '00af87', '00afaf', '00afd7', '00afff', // 40
+      '00d700', '00d787', '00d787', '00d7af', '00d7af', '00d7ff', '00ff00', '00ff5f', '00ff87', '00ffaf', // 50
+      '00ffd7', '00ffff', '5f0000', '5f5fff', '5f0087', '5f00af', '5f00d7', '5f00ff', '5f5f00', '5f5f5f', // 60
+      '5f5f87', '5f5faf', '5f5fd7', '5f5fff', '5f8700', '5f875f', '5f8787', '5f87af', '5f87d7', '5f87ff', // 70
+      '5faf00', '5faf5f', '5faf87', '5fafaf', '5fafd7', '5fafff', '5fd700', '5fd75f', '5fd787', '5fd7af', // 80
+      '5fd7d7', '5fd7ff', '5fff00', '3399cc', '5fff87', '5fffaf', '5fffd7', '5fffff', '870000', '87005f', // 90
+      '870087', '8700af', '8700af', '8700ff', '875f00', '875f5f', '875f87', '875faf', '875fd7', '875fff', // 100 
+      '878700', '87875f', '878787', '8787af', '8787d7', '8787ff', '87af00', '87af5f', '87af87', '87afaf', // 110
+      '87afd7', '87afff', '87d700', '87d75f', '87d787', '87d7af', '87d7d7', '87d7ff', '87ff00', '87ff5f', // 120
+      '87ff87', '87ffaf', '87ffd7', '87ffff', 'af0000', 'af005f', 'af0087', 'af00af', 'af00d7', 'af00ff', // 130
+      'af5f00', 'af5f5f', 'af5f87', 'af5faf', 'af5fd7', 'af5fff', 'af8700', 'af875f', 'af8787', 'af87af', // 140
+      'af87d7', 'af87ff', 'afaf00', 'afd7af', 'afaf87', 'afafaf', 'afafd7', 'afafff', 'afd700', 'afd75f', // 150
+      'afd787', 'afd7af', 'afd7d7', 'afd7ff', 'afff00', 'afff5f', 'afff87', 'afffaf', 'afffd7', 'afffff', // 160
+      'd70000', 'd7005f', 'dd2699', 'd700af', 'd700d7', 'd700ff', 'd75f00', 'd75f5f', 'd75f87', 'd75faf', // 170
+      'd75fd7', 'd75fff', 'd78700', 'd7875f', 'd78787', 'd787af', 'd787d7', 'd787ff', 'd7af00', 'd7af5f', // 180
+      'd7af87', 'd7afaf', 'd7afd7', 'd7afff', 'd7d75f', 'd7d75f', 'd7d787', 'd7d7af', 'd7d7d7', 'd7d7ff', // 190
+      'd7ff00', 'd7ff5f', 'd7ff87', 'd7ffaf', 'd7ffd7', 'd7ffff', 'ff0000', 'ff005f', 'ff0087', 'ff00af', // 200
+      'ff00d7', 'ff00ff', 'ff5f00', 'ff5f5f', 'ff5f87', 'ff5faf', 'ff5fd7', 'ff5fff', 'ff8700', 'ff875f', // 210
+      'ff8787', 'ff87af', 'ff87d7', 'ffaf00', 'ffaf00', 'ffaf5f', 'ffaf87', 'ffafaf', 'ffafd7', 'ffafff', // 220
+      'ffd700', 'ffd75f', 'ffd787', 'ffd7af', 'ffd7d7', 'ffd7ff', 'ffff00', 'ffff5f', 'ffff87', 'ffffaf', // 230
+      'ffffd7', 'ffffff', '080808', '121212', '1c1c1c', '262626', '303030', '3a3a3a', '444444', '4e4e4e', // 240
+      '585858', '626262', '6c6c6c', '767676', '808080', '8a8a8a', '949494', '9e9e9e', 'a8a8a8', 'b2b2b2', // 250
+      'bcbcbc', 'c6c6c6', 'd0d0d0', 'e4e4e4', 'e4e4e4', 'eeeeee', 'ffffff'
+    ];
 
 function Codepage(codepageUrl, callback) {
-        var COLORS, img, codepageImgs, backgroundImgs;
-
-        COLORS = [[0, 0, 0], [170, 0, 0], [0, 170, 0], [170, 85, 0], [0, 0, 170], [170, 0, 170], [0, 170, 170], [170, 170, 170], [85, 85, 85], [255, 85, 85], [85, 255, 85], [255, 255, 85], [85, 85, 255], [255, 85, 255], [85, 255, 255], [255, 255, 255]];
+        var img, codepageImgs, backgroundImgs;
 
         function createCanvas(width, height) {
             var newCanvas;
@@ -64,6 +91,7 @@ function h2d(h) {return parseInt(h,16);}
 
         img = new Image();
         img.onload = function () {
+       
             var i, background;
             characterWidth = img.width / 32;
             
@@ -72,13 +100,12 @@ function h2d(h) {return parseInt(h,16);}
             backgroundImgs = [];
             background = createCanvas(characterWidth, characterHeight);
             for (i = 0; i < ansicolors.length; i++) {
+                document.getElementById('progress').innerHTML="i: "+i+" of "+ansicolors.length;
                 codepageImgs[i] = colorCanvas(img, i, true);
                 backgroundImgs[i] = colorCanvas(background, i, false);
             }
             
             newCanvas = document.createElement("canvas");
-            
-            
             
             newCanvas.setAttribute("width", img.width*16);
             newCanvas.setAttribute("height", img.height*16);
@@ -96,7 +123,7 @@ function h2d(h) {return parseInt(h,16);}
             }
             
             document.body.appendChild(newCanvas);
-            alert("OK");
+
             callback();
         };
         img.src = codepageUrl;
